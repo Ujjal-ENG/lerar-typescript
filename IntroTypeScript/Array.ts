2422a8b85670dbfcb0236cc2e3cc929979a5fc16
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-27 19:48:30 +0600

    array typescript is done

---
+++ IntroTypeScript/Array.ts
@@ -0,0 +1,16 @@
+const superHeros: number[] = [];
+const heroName: Array<string> = [];
+
+superHeros.push(1);
+heroName.push("SpiderMan");
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MlModels: number[][] = [[255, 255, 255], []];
+
+allUsers.push({ name: "Kopa", isActive: false });
