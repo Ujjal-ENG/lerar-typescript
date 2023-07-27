3765d4afcb2618dba30eab061fb806ddbbcd5cd8
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-27 22:15:33 +0600

    done the tupples

---
+++ IntroTypeScript/Tuples.ts
@@ -0,0 +1,8 @@
+// const user: (string | number)[] = ["ukr", 1, 2];
+let tuppleUser: [string, number, boolean];
+
+tuppleUser = ["jio", 3, false];
+
+type User = [number, string];
+
+const newUser: User = [11, "hello"];
