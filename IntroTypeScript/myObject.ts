067a278b6245d64b77c33da37f9942254bded92a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-27 19:18:46 +0600

    obejct type set done

--- IntroTypeScript/myObject.ts
+++ IntroTypeScript/myObject.ts
@@ -1,28 +1,59 @@
-const User = {
-  name: "Ujjal",
-  email: "kopa@gmail.com",
-  isActive: true,
-};
+// const User = {
+//   name: "Ujjal",
+//   email: "kopa@gmail.com",
+//   isActive: true,
+// };
 
-function createUser({ name: string, isPaid: boolean }) {}
+// function createUser({ name: string, isPaid: boolean }) {}
 
-let newUser = { name: "Ujjal", isPaid: false, email: "h@h.com" };
+// let newUser = { name: "Ujjal", isPaid: false, email: "h@h.com" };
 
-createUser(newUser);
+// createUser(newUser);
 
-function createCourses(): { name: string; age: number } {
-  return { name: "ujjal", age: 23 };
-}
+// function createCourses(): { name: string; age: number } {
+//   return { name: "ujjal", age: 23 };
+// }
 
-type User = {
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+
+// function createUser2(user: User): User {
+//   return { name: "", email: "", isActive: true };
+// }
+// createUser2({ name: "", email: "", isActive: true });
+
+type User2 = {
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
+  creaditCardDetails?: number;
 };
 
-function createUser2(user: User): User {
-  return { name: "", email: "", isActive: true };
-}
-createUser2({ name: "", email: "", isActive: true });
+let myUser: User2 = {
+  _id: "1234",
+  name: "Jio",
+  email: "j@j.com",
+  isActive: false,
+};
+
+type cardNumber = {
+  cardnumber: string;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
+
+myUser.email = "t@t.com";
+// myUser._id = "232";
 
 export {};
