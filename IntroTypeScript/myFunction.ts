faf78fd59a3a7bb73201381045cb3f50822f2928
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-27 17:37:58 +0600

    learnt the typescript function types setup

--- IntroTypeScript/myFunction.ts
+++ IntroTypeScript/myFunction.ts
@@ -1,10 +1,42 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
   return num + 2;
 }
 
-function getUpper(val: number) {
+function getUpper(val: string) {
   return val.toUpperCase();
 }
 
 addTwo(4);
 getUpper("eo");
+// default value
+let loginUser = (name: string, email: string, isPaid: boolean = true) => {
+  return { name, email, isPaid };
+};
+
+loginUser("ujjal", "kopa");
+
+function getValue(myVal: number): any {
+  if (myVal > 5) {
+    return true;
+  }
+  return "200 OK";
+}
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errMsg: string): void {
+  console.log(errMsg);
+}
+function handleError(errMsg: string): never {
+  throw new Error(errMsg);
+}
+
+export {};
