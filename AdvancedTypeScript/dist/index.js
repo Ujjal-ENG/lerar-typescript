f14da4c4e4bfbef8ece8cfadeec1c7f76d625c3b
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-28 23:33:40 +0600

    learnt the access modifirr in typescript

--- AdvancedTypeScript/dist/index.js
+++ AdvancedTypeScript/dist/index.js
@@ -3,8 +3,24 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.email = email;
         this.name = name;
+    }
+    deleteToken() {
+        console.log("Token Deleted");
+    }
+    get getAppleEmail() {
+        return `Apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course Count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const obj1 = new User("u@u.com", "ujjal");
--- AdvancedTypeScript/src/index.ts
+++ AdvancedTypeScript/src/index.ts
@@ -1,8 +1,27 @@
 class User {
+  private _courseCount = 1;
   private readonly city: string;
   constructor(public email: string, public name: string) {
     this.email = email;
     this.name = name;
+  }
+
+  private deleteToken() {
+    console.log("Token Deleted");
+  }
+  get getAppleEmail(): string {
+    return `Apple ${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course Count should be more than 1");
+    }
+    this._courseCount = courseNum;
   }
 }
 
