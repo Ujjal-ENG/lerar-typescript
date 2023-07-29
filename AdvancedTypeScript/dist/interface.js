d8574eee30cd9ec130002f523af4ea24a03d65b6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-29 11:56:15 +0600

    now i will learn abstraction in typescript

--- AdvancedTypeScript/dist/interface.js
+++ AdvancedTypeScript/dist/interface.js
@@ -1,1 +1,9 @@
 "use strict";
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    createStory() { }
+}
