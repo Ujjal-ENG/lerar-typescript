a93431858a4207d4489777ae704be07ee798544c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-08-06 17:41:44 +0600

    added another async await call

--- TypeScriptPractice/FunctionProblem.js
+++ TypeScriptPractice/FunctionProblem.js
@@ -1,5 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 function Even(num) {
     return num % 2 === 0;
 }
 console.log(Even(5));
+let nums = [2, 4, 3, 32, 34];
+const map2 = nums.map((el) => el * 2);
+console.log(map2);
+function FetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield fetch("https://jsonplaceholder.typicode.com/todos/1");
+        const data = yield res.json();
+        console.log(data);
+    });
+}
+FetchData();
--- TypeScriptPractice/FunctionProblem.ts
+++ TypeScriptPractice/FunctionProblem.ts
@@ -3,3 +3,16 @@ function Even(num: number): boolean {
 }
 
 console.log(Even(5));
+
+let nums: Array<number> = [2, 4, 3, 32, 34];
+const map2 = nums.map((el) => el * 2);
+
+console.log(map2);
+
+async function FetchData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await res.json();
+  console.log(data);
+}
+
+FetchData();
