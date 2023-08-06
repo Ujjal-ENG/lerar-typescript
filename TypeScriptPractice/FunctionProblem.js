cb2e0bff2cebf3c139a82eaf8f56eba5959be131
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-08-06 16:31:12 +0600

    solved the first problem

---
+++ TypeScriptPractice/FunctionProblem.js
@@ -0,0 +1,8 @@
+"use strict";
+function Even(num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+console.log(Even(2));
---
+++ TypeScriptPractice/FunctionProblem.ts
@@ -0,0 +1,8 @@
+function Even(num: number): boolean {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(Even(2));
